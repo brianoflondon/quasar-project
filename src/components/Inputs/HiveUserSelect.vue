4e7af9368dd3db364fe0de976face7d1f9a666a8
Brian of London (Dot) <brian@v4v.app>
2023-04-20 14:28:32 +0300

    rebuilding hiveuserselect
    Signed-off-by: Brian of London (Dot) <brian@v4v.app>

--- src/components/Inputs/HiveUserSelect.vue
+++ src/components/Inputs/HiveUserSelect.vue
@@ -4,7 +4,7 @@
       <div class="col-8 q-pr-md">
         <q-select
           clearable
-          autocomplete
+          autocomplete="on"
           hide-selected
           :value="input"
           use-input
@@ -18,41 +18,23 @@
           @filter-abort="abortFilterFn"
           @input-value="virtualScroll"
           @input="$emit('selectUsername', input)"
+          autofocus="true"
         />
-      </div>
-      <div class="col-4">
-        <q-card class="my-card">
-          <q-img :src="avatarIRI" basic>
-            <div
-              v-if="fullName"
-              class="my-text absolute-bottom text-subtitle2 text-center"
-            >
-              {{ fullName }}
-            </div>
-          </q-img>
-        </q-card>
       </div>
     </div>
   </div>
 </template>
 
-<style lang="sass" scoped>
-.my-card
-  width: 100%
-  max-width: 200px
-
-.my-text
-  padding: 1px
-</style>
-
 <script setup>
+import { ref, defineComponent } from 'vue'
+import { useStoreUser } from 'src/stores/storeUser'
 import badActorList from '@hiveio/hivescript/bad-actors.json'
-import {defineComponent, ref } from 'vue'
-import 'src/assets/hive-tx.min.js'
 
-defineComponent({
-  name: 'HiveUserSelect',
-})
+const storeUser = useStoreUser()
+
+const input = ref('')
+const usernameSuggestions = ref([])
+const badActors = ref(badActorList)
 
 const props = defineProps({
   label: {
@@ -60,105 +42,28 @@ const props = defineProps({
     default: '',
   },
   useLoggedInUser: {
-    type: boolean,
-    default: '',
+    type: Boolean,
+    default: false,
   },
 })
 
-const emits = defineEmits(['hiveAccname'])
-
-watch (input, (newState) => {
-  this.input = newState
-})
-
-</script>
-<!--
-<script>
-import badActorList from '@hiveio/hivescript/bad-actors.json'
-export default {
-  name: 'userSearchBox',
-  data() {
-    return {
-      input: this.username,
-      fullName: '',
-      usernameSuggestions: [],
-      badActors: badActorList,
-      vscrollAcc: null,
-    }
-  },
-  computed: {
-    avatarIRI: function () {
-      if (this.vscrollAcc && this.vscrollAcc.length > 2) {
-        const re =
-          /^(?=.{3,16}$)[a-z]([0-9a-z]|[0-9a-z-](?=[0-9a-z])){2,}([.](?=[a-z][0-9a-z-][0-9a-z-])[a-z]([0-9a-z]|[0-9a-z-](?=[0-9a-z])){1,}){0,}$/
-        const validName = this.vscrollAcc.match(re)
-        if (validName) {
-          return 'https://images.hive.blog/u/' + this.vscrollAcc + '/avatar'
-        } else {
-          return null
-        }
-      } else {
-        return 'unkown_hive_user.png'
-      }
-    },
-  },
-  props: ['username', 'label'],
-  watch: {
-    username: function (newState) {
-      this.input = newState
-    },
-  },
-  methods: {
-    filterFn(val, update, abort) {
-      update(() => {
-        this.searchHiveUsernames(val)
-      })
-    },
-    virtualScroll(obj) {
-      // Sets the value of the selected item
-      this.$hive.api.getAccounts([obj], (err, data) => {
-        if (err) {
-          console.error(err)
-        }
-        if (data[0] && data[0].posting_json_metadata) {
-          this.fullName = JSON.parse(data[0].posting_json_metadata).profile.name
-        } else {
-          this.fullName = ''
-        }
-      })
-      this.vscrollAcc = obj
-    },
-    abortFilterFn() {},
-    searchHiveUsernames(val) {
-      if (val.length < 2) {
-        return
-      }
-      const partialusername = val.toLowerCase()
-
-      this.$hive.api.getAccountReputationsAsync(
-        partialusername,
-        5,
-        (err, data) => {
-          if (err) {
-            console.error(err)
-          } else {
-            const accounts = data
-              .map((el) => {
-                return el.account
-              })
-              .filter((el) => {
-                return this.badActors.indexOf(el) === -1
-              })
-            this.usernameSuggestions = accounts
-          }
-        }
-      )
-    },
-    clearInput() {
-      this.input = ''
-      this.fullName = ''
-      this.usernameSuggestions = []
-    },
-  },
+if (props.useLoggedInUser) {
+  input.value = storeUser.username
 }
-</script> -->
+
+function filterFn() {
+  console.log('filterFn')
+}
+
+function abortFilterFn() {
+  console.log('abortFilterFn')
+}
+
+function virtualScroll(obj) {
+  console.log('virtualScroll', obj)
+}
+
+function clearInput() {
+  input.value = ''
+}
+</script>
