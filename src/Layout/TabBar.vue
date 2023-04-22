f13af69bd03ac1958b70607b4bb191550e79bebb
Brian of London (Dot) <brian@v4v.app>
2023-04-22 21:44:51 +0300

    colours changing on tab bar
    Signed-off-by: Brian of London (Dot) <brian@v4v.app>

--- src/Layout/TabBar.vue
+++ src/Layout/TabBar.vue
@@ -1,11 +1,25 @@
 <template>
-  <q-tabs no-caps class="bg-orange text-white shadow-2">
-    <q-route-tab to="/login" exact replace :label="$t('login')" />
-    <q-route-tab to="/transfer" exact replace :label="$t('transfer')" />
-    <q-route-tab to="/testing" exact replace :label="$t('testing')" />
-    <q-route-tab to="/images" exact replace :label="$t('images')" />
-    <q-route-tab to="/status" exact replace :label="$t('status')" />
-  </q-tabs>
+  <div class="tab-bar-top">
+    <q-tabs no-caps class="shadow-2">
+      <q-route-tab to="/login" exact replace :label="$t('login')" />
+      <q-route-tab to="/transfer" exact replace :label="$t('transfer')" />
+      <q-route-tab to="/testing" exact replace :label="$t('testing')" />
+      <q-route-tab to="/images" exact replace :label="$t('images')" />
+      <q-route-tab to="/status" exact replace :label="$t('status')" />
+    </q-tabs>
+  </div>
 </template>
 
 <script setup></script>
+
+<style lang="scss">
+.tab-bar-top {
+  background-color: orange;
+  color: #03002c;
+}
+
+.body--dark .tab-bar-top {
+  background-color: #03002c;
+  color: #1876d2;
+}
+</style>
